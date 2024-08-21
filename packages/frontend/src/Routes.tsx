f42862de14912e24089fdd3e0333e87ba1b899da
Bob Scott <bobscott78@gmail.com>
2024-08-21 15:44:36 +0100

    Add not found route

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./containers/Home.tsx";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      {/* Finally, catch all unmatched routes */}
+      <Route path="*" element={<NotFound />} />;
     </Routes>
   );
 }
