b48c43a67c4f4d801adaaea246548440fe6e1181
Bob Scott <bobscott78@gmail.com>
2024-08-25 13:44:07 +0100

    Add login form

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
